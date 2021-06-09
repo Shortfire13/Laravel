257a17b365150f9bd4b11b6a3f4b326dc40940b8
novrinjr <novrinjr@gmail.com>
2021-06-09 23:10:52 +0700

    update template frontend

--- bank_sampah/resources/views/frontend/layout/main.blade.php
+++ bank_sampah/resources/views/frontend/layout/main.blade.php
@@ -10,23 +10,23 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="frontend/assets/img/favicon.png" rel="icon">
+  <link href="frontend/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="frontend/assets/vendor/aos/aos.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="frontend/assets/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Serenity - v2.2.1
@@ -140,7 +140,7 @@
         <div class="row">
 
           <div class="col-lg-6 video-box align-self-baseline">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+            <img src="frontend/assets/img/about.jpg" class="img-fluid" alt="">
             <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           </div>
 
@@ -312,19 +312,19 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="frontend/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="frontend/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="frontend/assets/vendor/php-email-form/validate.js"></script>
+  <script src="frontend/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="frontend/assets/vendor/counterup/counterup.min.js"></script>
+  <script src="frontend/assets/vendor/venobox/venobox.min.js"></script>
+  <script src="frontend/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="frontend/assets/vendor/aos/aos.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="frontend/assets/js/main.js"></script>
 
 </body>
 
--- bank_sampah/routes/web.php
+++ bank_sampah/routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('home', function () {
-    return view('layout/main');
+    return view('frontend/layout/main');
 });
