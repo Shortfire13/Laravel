644b75da9561829ab6b325db9186f31c82386327
novrinjr <novrinjr@gmail.com>
2021-06-09 23:38:19 +0700

    Templating Admin

--- bank_sampah/routes/web.php
+++ bank_sampah/routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('home', function () {
     return view('frontend/layout/main');
 });
+Route::get('admin', function () {
+    return view('backend/layouts/main');
+});
