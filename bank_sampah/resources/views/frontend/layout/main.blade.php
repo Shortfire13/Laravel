e356fe568b16a18fa0bf2e57049c0143086f0e56
novrinjr <novrinjr@gmail.com>
2021-06-09 23:15:08 +0700

    Update main.blade.php

--- bank_sampah/resources/views/frontend/layout/main.blade.php
+++ bank_sampah/resources/views/frontend/layout/main.blade.php
@@ -339,21 +339,6 @@
 
   <!-- Template Main JS File -->
   <script src="frontend/assets/js/main.js"></script>
-=======
-  <script src="{{ asset('frontend/assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/counterup/counterup.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/venobox/venobox.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{ asset('frontend/assets/vendor/aos/aos.js')}}"></script>
-
-  <!-- Template Main JS File -->
-  <script src="{{ asset('frontend/assets/js/main.js')}}"></script>
->>>>>>> cb61e2dafd37135d516c25614da3d60a29e4d072:bank_sampah/resources/views/layout/main.blade.php
 
 </body>
 
