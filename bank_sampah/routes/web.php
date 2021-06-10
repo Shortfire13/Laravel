a9882207eb061381a5382c70d40c486c1748a053
geofannyher <geofannyher@gmail.com>
2021-06-10 09:51:05 +0700

    loginandregisteruser

--- bank_sampah/routes/web.php
+++ bank_sampah/routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/admin', [AdminController::class, 'index']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
